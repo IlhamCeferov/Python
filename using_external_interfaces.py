ff10fc890da6b4d5819ba4372108b48be7ee3637
IlhamCeferov <ilham.ceferov@gmail.com>
2024-11-10 17:03:29 +0400

    Add response handling

--- using_external_interfaces.py
+++ using_external_interfaces.py
@@ -1,1 +1,14 @@
 import requests
+import json
+
+keyword = input("Enter keyword: ")
+request = "https://api.tvmaze.com/search/shows?q=" + keyword
+response = requests.get(request).json()
+#print(json.dumps(response, indent=2))
+
+for a in response:
+    show = a['show']
+    schedule = show["schedule"]
+    country_name = show["network"]["country"]["name"] if show["network"] is not None else "No country"
+
+    print(f"{show["name"]}: {country_name}: {schedule["days"]}: {schedule["time"]}")
